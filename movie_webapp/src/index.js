bfa2b02946c317ad8ff9c32c830f9c2a9a44ddb3
farooq <farooq.chilakala@gmail.com>
2023-06-11 16:06:01 +0530

    movieland

---
+++ movie_webapp/src/index.js
@@ -0,0 +1,20 @@
+
+import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { ScrollToTop } from './components';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <ScrollToTop />
+      <App />
+    </Router>
+  </React.StrictMode>
+);
+
+
---
+++ movie_webapp/src/pages/MovieList.js
@@ -0,0 +1,49 @@
+
+import { useFetch } from "../hooks/useFetch";
+import { useTitle } from "../hooks/useTitle";
+import { Card } from "../components";
+
+export const MovieList = ({ apiPath, title }) => {
+  const { data: movies } = useFetch(apiPath);
+  const pageTitle = useTitle(title);
+
+  if (!movies) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <main>
+      <h1>{pageTitle}</h1>
+      <section className="max-w-7xl mx-auto py-7">
+        <div className="flex justify-start flex-wrap other:justify-evenly">
+          {movies.map((movie) => (
+            <Card key={movie.id} movie={movie} />
+          ))}
+        </div>
+      </section>
+    </main>
+  );
+};
+
+
+// import { useFetch } from "../hooks/useFetch";
+// import { useTitle } from "../hooks/useTitle";
+// import { Card } from "../components";
+
+
+// export const MovieList = ({apiPath, title}) => {
+//   const { data: movies } = useFetch(apiPath);
+//   const pageTitle = useTitle(title);
+
+//   return (
+//     <main>
+//       <section className="max-w-7xl mx-auto py-7">
+//         <div className="flex justify-start flex-wrap other:justify-evenly">       
+//           { movies.map((movie) => (
+//             <Card key={movie.id} movie={movie} />
+//           )) }          
+//         </div>
+//       </section>
+//     </main>
+//   )
+// }
